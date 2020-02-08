839e381c4c1e9c94e4b6deeddffc6deee410da60
Martand Singh <martandsays@gmail.com>
2020-02-09 00:05:45 +0400

    New changes done today

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ const http = require('http')
 http.createServer((req, res) => {
     console.log(req.url)
     if(req.url === '/'){
-        res.write("Hello World From Node Js")
+        res.write("Hello World From Node Js By martand singh")
         res.end();
     }
     else if(req.url === '/cicd'){
-        res.write('Welcome to Azure CI CD pipeline')
+        res.write('Welcome to Azure CI CD pipeline - Updated')
         res.end()
     }
 }).listen(3000, () =>{
